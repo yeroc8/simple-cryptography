35cdd60eac1861333a32ba27788e74568e1314b9
yeroc8 <coreyshive@MacBook-Pro.local>
2019-04-07 15:13:25 -0400

    Implement basic CLI
    Probably doesn’t actually run

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,56 @@
 import org.apache.commons.cli.*;
-import java.nio.file.*;
+import org.apache.commons.io.IOUtils;
+import org.mozilla.universalchardet.UniversalDetector;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.*;
 import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws ParseException, IOException {
+
+    public static void main(String[] args) {
+        try {
+            run(args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            main(new String[] {"-h"});
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void run(String[] args) throws ParseException, IOException {
         Options options = new Options();
+        options.addOption("h", "help", false, "display this message");
+        options.addOption(Option.builder("k").longOpt("key").hasArg().desc("cipher key").required().build());
         options.addOption("d", "decode", false, "decode input instead of encode");
         options.addOption("i", "input", true, "input file [default: stdin]");
         options.addOption("o", "output", true, "output file [default: stdout]");
-
         CommandLine cli = new DefaultParser().parse(options, args);
+        if (cli.hasOption('h')) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("simple-cryptography", options);
+        }
         byte[] input;
         if (cli.hasOption('i')) {
             input = Files.readAllBytes(Paths.get(cli.getOptionValue('i')));
         } else {
-            // stdin
+            input = IOUtils.toByteArray(System.in);
         }
-        byte[] output = new byte[0];
+        UniversalDetector detector = new UniversalDetector();
+        detector.handleData(input);
+        detector.dataEnd();
+        String encoding = detector.getDetectedCharset();
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
+        Cipher cipher = new Cipher(new String(input, encoding), cli.getOptionValue('k'));
+        cipher.transpose(cli.hasOption('d'));
         if (cli.hasOption('o')) {
-            Files.write(Paths.get(cli.getOptionValue('o')), output, StandardOpenOption.CREATE);
+            Files.write(Paths.get(cli.getOptionValue('o')), cipher.getText().getBytes(), CREATE);
         } else {
-            System.out.println(new String(output));
+            System.out.println(cipher.getText());
         }
     }
 }
