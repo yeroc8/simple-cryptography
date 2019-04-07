dfe716e20ba63e456c2a1812caf809c75d187ce5
yeroc8 <coreyshive@MacBook-Pro.local>
2019-04-07 12:02:57 -0400

    Start CLI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import org.apache.commons.cli.*;
+
 public class Main {
     public static void main(String[] args) {
-        // do stuff
+        Options options = new Options();
+        options.addOption("d", "decode", false, "decode input instead of encode");
+        options.addOption("i", "input", true, "input file [default: stdin]");
+        options.addOption("o", "output", true, "output file [default: stdout]");
+        try {
+            CommandLine cli = new DefaultParser().parse(options, args);
+            if (cli.hasOption('i')) {
+                // input file
+            } else {
+                // stdin
+            }
+            if (cli.hasOption('o')) {
+                // output file
+            } else {
+                // stdout
+            }
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
