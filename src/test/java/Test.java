b50fe4e81a2a2a795e128416dccbbd91d9c25869
yeroc8 <coreyshive@MacBook-Pro.local>
2019-04-15 20:53:07 -0400

    Clean up yet another push full of crap
    For the love of god, please don’t push your compiled classes!

--- src/test/java/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-
-   public static void main(String[] args) {
-      Base32 test = new Base32();
-      System.out.println(test.encode("a"));
-      System.out.println(test.decode("a"));
-   }
-}
